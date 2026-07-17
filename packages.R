fbf91eea3ba64e32fc3f3a3e8a3313a20d722787
wtassinari <wtassinari@gmail.com>
2026-07-17 05:45:18 -0300

    Add packages.R for Connect dependency detection

---
+++ packages.R
@@ -0,0 +1,19 @@
+# packages.R
+# Arquivo de dependências explícitas para Posit Connect Cloud
+# O Connect usa este arquivo para detectar e instalar automaticamente todos os pacotes necessários
+
+library(tidyverse)
+library(tidyr)
+library(DT)
+library(scales)
+library(shiny)
+library(shinydashboard)
+library(plotly)
+library(shinyWidgets)
+library(ggplot2)
+library(dplyr)
+library(readr)
+library(purrr)
+library(tibble)
+library(stringr)
+library(forcats)
